ea9af9292e132cf0953d5f68d80acd3c70dae3db
Patrick Heck <patrickbenjaminheck@gmail.com>
2020-02-18 16:09:08 +0000

    first 5 behaviours implemented

--- src/main/java/com/develogical/camera/Camera.java
+++ src/main/java/com/develogical/camera/Camera.java
@@ -2,16 +2,39 @@ package com.develogical.camera;
 
 public class Camera {
 
+    private final Sensor sensor;
+    private final MemoryCard memorycard;
+    private WriteCompleteListener listener;
+    private boolean powerStatus;
+    private boolean writing;
+
+    public Camera(Sensor sensor, MemoryCard memorycard, WriteCompleteListener listener) {
+        this.sensor = sensor;
+        this.memorycard = memorycard;
+        this.listener = listener;
+        this.powerStatus = false;
+        this.writing = false;
+    }
+
     public void pressShutter() {
-        // not implemented
+        if(this.powerStatus){
+            byte[] sensorData  = this.sensor.readData();
+            this.writing = true;
+            this.memorycard.write(sensorData,this.listener);
+        }
     }
 
     public void powerOn() {
-        // not implemented
+        this.powerStatus = true;
+        this.sensor.powerUp();
     }
 
     public void powerOff() {
-       // not implemented
+        if(!this.writing){
+        this.powerStatus = false;
+        this.sensor.powerDown();
+        }
     }
+
 }
 
--- src/test/java/com/develogical/camera/CameraTest.java
+++ src/test/java/com/develogical/camera/CameraTest.java
@@ -2,12 +2,63 @@ package com.develogical.camera;
 
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 
 import static org.mockito.Mockito.*;
 
 public class CameraTest {
+    Sensor mocksensor = mock(Sensor.class);
+    MemoryCard memcard = mock(MemoryCard.class);
+    WriteCompleteListener mockListener = mock(WriteCompleteListener.class);
+    Camera cam = new Camera(mocksensor, memcard, mockListener);
+
+
     @Test
     public void switchingTheCameraOnPowersUpTheSensor() {
-        // write your test here
+        cam.powerOn();
+        verify(mocksensor).powerUp();
     }
+    @Test
+    public void switchingTheCameraOffPowersDownTheSensor() {
+        cam.powerOff();
+        verify(mocksensor).powerDown();
+    }
+
+    @Test
+    public void pressingShutterCopiesData() {
+        Sensor mocksensor1 = mock(Sensor.class);
+        MemoryCard memcard1 = mock(MemoryCard.class);
+        WriteCompleteListener mockListener1 = mock(WriteCompleteListener.class);
+        when(mocksensor1.readData()).thenReturn(new byte[]{42});
+        Camera cam1 = new Camera(mocksensor1, memcard1, mockListener1);
+        cam1.powerOn();
+        cam1.pressShutter();
+        verify(memcard1).write(eq(new byte[]{42}), any());
+        //Why does moving when to the top work?
+    }
+    @Test
+    public void pressingShutterWhenPowerOffDoesNothing(){
+        cam.powerOff();
+        cam.pressShutter();
+        verify(mocksensor,never()).readData();
+        verify(memcard,never()).write(mocksensor.readData(), mockListener);
+    }
+
+    @Test
+    public void doesNotSwitchOffWhenWritingData(){
+        cam.powerOn();
+        cam.pressShutter();
+        cam.powerOff();
+        verify(mocksensor,never()).powerDown();
+    }
+
+//    @Test
+//    public void shutsDownOnceWriteComplete(){
+//        cam.powerOn();
+//        cam.pressShutter();
+//        cam.powerOff();
+//
+//
+//    }
+
 }
